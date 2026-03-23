db72b61f74b0374e2c21a791337f26422b5414eb
farmer_2010 <155635633+farmer2010@users.noreply.github.com>
2026-03-23 18:51:54 +1000

    Add files via upload

---
+++ farmgui/component.py
@@ -0,0 +1,57 @@
+from farmgui.input_manager import *
+import pygame
+pygame.init()
+
+class Component():
+    def __init__(self, rect, center=(0, 0)):
+        self.rect = pygame.Rect(rect)
+        self.center = center
+        if self.center == None: self.center = (0, 0)
+        self.input_manager = input_manager
+        self.image = pygame.Surface((self.rect.w, self.rect.h), pygame.SRCALPHA)
+        self.parent = None
+        self.rect.x -= self.rect.w * self.center[0]
+        self.rect.y -= self.rect.h * self.center[1]
+        self.absolute_pos = [0, 0]
+        self.is_panel = 0
+
+    def update_component(self, events):
+        self.update(events)
+        mousedown = pygame.mouse.get_pressed()[0]
+        mouse_collide = self.collide()
+        if mouse_collide:
+            if mousedown:
+                if self.input_manager.mousetag_object[0] == None:
+                    self.input_manager.mousetag_object[0] = self
+            else:
+                if self.input_manager.mousetag_object[0] == self:
+                    self.input_manager.mousetag_object[0] = None
+        else:
+            if not mousedown:
+                if self.input_manager.mousetag_object[0] == self:
+                    self.input_manager.mousetag_object[0] = None
+
+    def get_mousepos(self):
+        mousepos = pygame.mouse.get_pos()
+        mpos = (mousepos[0] - self.parent.absolute_pos[0], mousepos[1] - self.parent.absolute_pos[1])#позиция мыши относительно родительского компонента
+        return(mpos)
+
+    def collide(self):
+        mousepos = self.get_mousepos()
+        return((mousepos[0] >= self.rect.x and mousepos[0] <= self.rect.x + self.rect.w) and (mousepos[1] >= self.rect.y and mousepos[1] <= self.rect.y + self.rect.h))
+
+    def set_absolute_pos(self):
+        if self.parent != None:
+            self.absolute_pos = [
+                self.parent.absolute_pos[0] + self.rect.x,
+                self.parent.absolute_pos[1] + self.rect.y
+            ]
+        if self.is_panel:
+            for c in self.buttons:
+                c.set_absolute_pos()
+
+    def update(self, events):
+        pass
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.rect.x, self.rect.y))
---
+++ farmgui/panel.py
@@ -0,0 +1,73 @@
+from farmgui.component import *
+import pygame
+pygame.init()
+
+class Panel(Component):
+    def __init__(self, rect, background_color=(50, 50, 50), is_main=0, **kwargs):
+        Component.__init__(self, rect, kwargs.get("center"))
+        self.background_image = kwargs.get("background_image")
+        if kwargs.get("background_image") == None:
+            self.background_image = pygame.Surface((self.rect.w, self.rect.h), pygame.SRCALPHA)
+            self.background_image.fill(background_color)
+        self.buttons = []
+        self.is_main = is_main
+        self.first_upd = 1
+        self.is_panel = 1
+
+    def update_component(self, events):
+        if self.is_main and self.first_upd:
+            self.first_upd = 0
+            self.set_absolute_pos()
+        self.update(events)
+        mousedown = pygame.mouse.get_pressed()[0]
+        mouse_collide = self.collide()
+        if mouse_collide:
+            if mousedown:
+                if self.input_manager.mousetag_object[0] == None:
+                    press_button = 0
+                    for b in self.buttons:
+                        m_c = b.collide()
+                        press_button = m_c
+                        if m_c:
+                            break
+                    if not press_button:
+                        self.input_manager.mousetag_object[0] = self
+            else:
+                if self.input_manager.mousetag_object[0] == self:
+                    self.input_manager.mousetag_object[0] = None
+        else:
+            if not mousedown:
+                if self.input_manager.mousetag_object[0] == self:
+                    self.input_manager.mousetag_object[0] = None
+        for b in self.buttons:
+            b.update_component(events)
+
+    def draw(self, screen):
+        self.image.blit(self.background_image, (0, 0))
+        for b in self.buttons:
+            b.draw(self.image)
+        screen.blit(self.image, self.rect)
+
+    def add(self, b):
+        self.buttons.append(b)
+        b.parent = self
+
+    def remove(self, b):
+        self.buttons.remove(b)
+
+    def get_mousepos(self):
+        mousepos = pygame.mouse.get_pos()
+        if self.is_main == 0:
+            mpos = (mousepos[0] - self.parent.rect.x, mousepos[1] - self.parent.rect.y)#позиция мыши относительно родительского компонента
+            return(mpos)
+        else:
+            return(mousepos)
+
+    def get_component(self, index):
+        return(self.buttons[index])
+
+    def get_component_count(self):
+        return(len(self.buttons))
+
+    def get_screen(self):
+        return(self.image)
